c0bab300c8068aa420570b2d658f8e83bfc4cd3f
Robert Mock <robertjamesmock@gmail.com>
2016-04-19 19:32:11 -0700

    running commands does indeed work (and is now tested)

--- Assets/talkin/Yarn Spinner/DialogueCharacter.cs
+++ Assets/talkin/Yarn Spinner/DialogueCharacter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class DialogueCharacter : ScriptableObject {
 
     
-    public Color textColor;//used in similar ways: font, color, border style, whatever
+    public Color textColor = new Color(1,0,1);//used in similar ways: font, color, border style, whatever
     public Font textFont;
 
 
